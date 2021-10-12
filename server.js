c945d7e0ff14a5dddb9667b4ef8fb626596c6198
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2021-10-12 15:15:45 -0500

    Second Commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/styles', express.static(path.join(__dirname, './public/styles.css')))
 try {
     nonExistentFunction();
 } catch (error) {
-    console.error(error);
+    console.error('error');
 }
 
 // app.use(rollbar.errorHandler())
